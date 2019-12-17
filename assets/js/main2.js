1453d4cd6766014b60fcbf76950fbe986c25d159
nogamba <nogamba@ucsd.edu>
2019-12-17 09:36:59 +0300

    changed design

--- assets/js/main2.js
+++ assets/js/main2.js
@@ -20,7 +20,7 @@
     /* ==========================================================================
        countdown timer
        ========================================================================== */
-     jQuery('#clock').countdown('2019/02/01',function(event){
+     jQuery('#clock').countdown('2020/01/24',function(event){
       var $this=jQuery(this).html(event.strftime(''
       +'<div class="time-entry days"><span>%-D</span> Days</div> '
       +'<div class="time-entry hours"><span>%H</span> Hours</div> '
